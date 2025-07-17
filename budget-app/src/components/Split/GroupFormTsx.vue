22520ad16a2fa65616a9b41e6063d179fa9f3f79
Tommaso Cont - IGW <tcont@infogestweb.it>
2025-07-17 12:49:59 +0200

    form aggiunta transazione ux

--- budget-app/src/components/Split/GroupFormTsx.vue
+++ budget-app/src/components/Split/GroupFormTsx.vue
@@ -8,6 +8,8 @@ const store = userStore()
 const name=ref()
 const desc = ref()
 const members = ref([])
+const activePay = ref()
+const input = ref([])
 
 function close() {
     store.form(false)
@@ -18,12 +20,17 @@ function data () {
   store.form(false)
 }
 
+function active(name) {
+    activePay.value = name
+    console.log(activePay.value)   
+}
+
 </script>
 
 <template>
-    <div class="flex flex-col w-85">
-        <div class="flex bg-gray-100 rounded-lg w-full p-6 h-120">
-            <div v-for="group in store.groupData" class="flex flex-col w-full gap-6">
+    <div class="flex flex-col w-90 max-h-180 overflow-y-auto">
+        <div class="flex bg-gray-100 rounded-lg w-full p-6">
+            <div v-for="group in store.groupData" class="flex flex-col w-full gap-2">
                 <div class="flex flex-row justify-between w-full">
                     <p class="font-bold text-lg">Nuova Spesa - {{ group.name }}</p>
                     <button>X</button>
@@ -42,11 +49,81 @@ function data () {
                         <option value="viaggi">Viaggi ✈️</option>
                         <option value="altro">Altre Spese 💶</option>
                     </select>
-                    <textarea class="border-1 border-gray-300 rounded-md h-10 p-4 text-neutral-500" name="" id="" placeholder="Descrizione (opzionale)">
-                    </textarea>
+                </div>
+                <textarea class="border-1 border-gray-300 rounded-md p-4 text-black h-20" name="" id="" placeholder="Descrizione (opzionale)"></textarea>
+                <label class="pt-5 text-lg font-semibold" for="chi">Chi ha pagato?</label>
+                <div v-for="member in group.members" :key="group.id">
+                    <button @click="active(member.name)" class="font-bold border-gray-300 border-1 p-2 rounded-lg w-full text-start"
+                    :class="activePay === member.name ? 'bg-neutral-800 text-white border-none' : 'bg-white'">{{ member.name }}</button>
+                </div>
+                <div class="flex flex-col gap-3">
+                    <p class="pt-5 text-lg font-semibold">Dividi Tra</p>
+                    <div v-for="member in group.members" :key="group.id" class="flex flex-row justify-between">
+                        <p>{{ member.name }}</p>
+                        <label class="switch">
+                        <input type="checkbox" :value="member.name" v-model="input">
+                        <span class="slider"></span>
+                        </label>
+                    </div>
+                    <div class="flex flex-row gap-2 pt-5">
+                        <button class="p-3 margin-1 margin-gray-300 w-full border-1 border-gray-300 rounded-lg font-bold" @click="close">Annulla</button>
+                        <button class="bg-black text-white rounded-lg p-3 w-full font-bold" @click="data()">Aggiungi Spesa</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
 </template>
+
+<style scoped>
+.switch {
+  font-size: 17px;
+  position: relative;
+  display: inline-block;
+  width: 46px;     /* Ridotto per proporzione */
+  height: 24px;    /* Altezza ridotta */
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #fff;
+  transition: .4s;
+  border-radius: 30px;
+  border: 1px solid #ccc;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 20px;    /* Un po' meno dell'altezza del contenitore */
+  width: 20px;
+  border-radius: 50%;
+  left: 2px;
+  top: 1px;
+  background-color: white;
+  box-shadow: 0 2px 5px #999999;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #000;
+  border: 1px solid transparent;
+}
+
+input:checked + .slider:before {
+  transform: translateX(22px);  /* Spostamento adattato */
+}
+
+</style>
--- budget-app/src/store/userStore.js
+++ budget-app/src/store/userStore.js
@@ -5,7 +5,7 @@ export const userStore = defineStore ('user', () => {
 
 const groupData = ref([])   
 const formActive = ref()
-const members = ref([{ name:'Antonio Florea', initials:'AF' }, { name:'Lorenzo Giacopuzzi', initials:'LG' },])
+const members = ref([{ name:'Antonio Florea', initials:'AF' }, { name:'Lorenzo Giacopuzzi', initials:'LG' }, { name:'Tommaso Chieregato', initials:'TC' }, { name:'Mattia Corolaita', initials:'MT' },])
 let id = 0;
 
 function form (data) {
@@ -14,8 +14,8 @@ function form (data) {
 
 function newGroup(name, desc, groupMembers) {
   const selectedMembers = members.value.filter(m =>
-    groupMembers.includes(m.name)
-  )
+    groupMembers.includes(m.name))
+    selectedMembers.push({name: 'Tu', initials: 'TU'})
 
   const newGroup = {
     id: id++,
